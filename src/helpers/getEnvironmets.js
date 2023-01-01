98088b6f18c82269889384c54a9bc9a72ba76c89
lucasgojeda <lukitas.ojeda.00@gmail.com>
2023-01-01 16:18:11 -0300

    Backend link changed, bug with environments solved.

--- src/helpers/getEnvironmets.js
+++ src/helpers/getEnvironmets.js
@@ -2,9 +2,8 @@
 
 export const getEnvironmets = () => {
 
-    import.meta.env;
-
     return {
-        ...import.meta.env
+        VITE_REACT_APP_API_URL: import.meta.env.VITE_REACT_APP_API_URL,
+        VITE_MODE: import.meta.env.VITE_MODE,
     }
 } 
